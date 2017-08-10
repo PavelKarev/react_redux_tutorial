8b94aa926a830590cfe8548f66130f03b5e43a19
Павел Карев <PavelKarev@iMac-Pavel.local>
2017-08-10 22:21:47 +0300

    added vk api

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './containers/App'
+import './styles/app.css' 
 import configureStore from './store/configureStore'
 
 const store = configureStore()
 
 render(
   <Provider store={store}>
-    <App />
+    <App className='app' />
   </Provider>,
   document.getElementById('root')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
+
 var path = require('path')
 var webpack = require('webpack')
-var NpmInstallPlugin = require('npm-install-webpack-plugin');
+var NpmInstallPlugin = require('npm-install-webpack-plugin')
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
@@ -17,11 +20,10 @@ module.exports = {
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
     new NpmInstallPlugin()
   ],
   module: {
-    preLoaders: [ //добавили ESlint в preloaders
+    preLoaders: [
       {
         test: /\.js$/,
         loaders: ['eslint'],
@@ -32,13 +34,20 @@ module.exports = {
     ],
     loaders: [
       {
-        loaders: ['react-hot', 'babel-loader'], //добавили loader 'react-hot'
+        loaders: ['react-hot', 'babel-loader'],
         include: [
           path.resolve(__dirname, "src"),
         ],
         test: /\.js$/,
         plugins: ['transform-runtime'],
+      },
+      {
+        test:   /\.css$/,
+        loader: "style-loader!css-loader!postcss-loader"
       }
     ]
+  },
+  postcss: function () {
+    return [autoprefixer, precss];
   }
 }
